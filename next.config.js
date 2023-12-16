ede74586b9a3207d3595d3059be539f10c6d151d
urateb <uratebekaj@gmail.com>
2023-12-16 22:56:45 +0100

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '',
 }
 
 module.exports = nextConfig
